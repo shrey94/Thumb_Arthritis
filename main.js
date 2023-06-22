4b923a57c23dc0e02946ee8e76cdbceae9f9493f
Mathur <shrey.94@hotmail.com>
2023-06-22 12:16:31 -0400

    shifted to main_2.js date function

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-
-const d = new Date();
-
-document.getElementById("Date_Info").innerHTML = monthNames[d.getMonth()] +'&nbsp' +d.getDate()+'<br>'+ 'Exercises';
 
 
 //REPS FOR EXERCISES
